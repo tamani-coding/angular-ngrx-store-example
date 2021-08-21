c746c1d04e5df923ecfe92f3c9c765fc5c4a08fc
genka <tamani.coding@gmail.com>
2021-08-21 16:07:23 +0200

    products list

---
+++ src/app/entity/product.ts
@@ -0,0 +1,8 @@
+export interface Product {
+
+  name: string,
+  description: string,
+  price: number,
+  image: string,
+
+}
--- src/app/shop-products/shop-products.component.ts
+++ src/app/shop-products/shop-products.component.ts
@@ -1,3 +1,4 @@
+import { Product } from './../entity/product';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -12,4 +13,22 @@ export class ShopProductsComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  public products(): Product[] {
+    return [ {
+      name: "Super Robot 2k",
+      description: "With Lase Guns!",
+      price: 40.99,
+      image: "assets/products/robot.png",
+    }, {
+      name: "Lucky Board Game",
+      description: "Up to 4 players!",
+      price: 35.00,
+      image: "assets/products/board_game.png",
+    }, {
+      name: "The Art of Videogames",
+      description: "Retro Edition. From more than 20 Artists!",
+      price: 21.15,
+      image: "assets/products/book.png",
+    }]
+  }
 }
