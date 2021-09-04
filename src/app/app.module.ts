db4424963a5f28163ce3c3e5032148bdacf5ee1e
genka <tamani.coding@gmail.com>
2021-09-04 11:37:42 +0200

    meta reducer for local storage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { cartReducer } from './cart-state-store/cart.reducer';
+import { cartReducer, metaReducerLocalStorage } from './cart-state-store/cart.reducer';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
@@ -18,7 +18,7 @@ import { ShopCartComponent } from './shop-cart/shop-cart.component';
   ],
   imports: [
     BrowserModule,
-    StoreModule.forRoot({ cartEntries: cartReducer }, {}),
+    StoreModule.forRoot({ cartEntries: cartReducer }, { metaReducers: [ metaReducerLocalStorage ] }),
     AppRoutingModule
   ],
   providers: [],
--- src/app/cart-state-store/cart.reducer.ts
+++ src/app/cart-state-store/cart.reducer.ts
@@ -1,5 +1,5 @@
 import { Product } from './../entity/product';
-import { createReducer, on } from '@ngrx/store';
+import { createReducer, on, ActionReducer, INIT, UPDATE } from '@ngrx/store';
 import { addProduct, clearCart, removeProduct } from './cart.actions';
 import { intialCartEntries } from './cart.state';
 
@@ -9,22 +9,39 @@ function filter(productId: number) {
 
 export const cartReducer = createReducer(
   intialCartEntries,
-  on(clearCart, entries => [] ),
-  on(addProduct, (entries, product) => {
 
+  on(clearCart, _ => [] ),
+
+  on(addProduct, (entries, product) => {
     const entriesClone: Product[] = JSON.parse(JSON.stringify(entries));
     entriesClone.push(product);
-
     return entriesClone;
   }),
-  on(removeProduct, (entries, product) => {
 
+  on(removeProduct, (entries, product) => {
     const entriesClone: Product[] = JSON.parse(JSON.stringify(entries));
     const found = entriesClone.find(filter(product.id));
     if (found) {
         entriesClone.splice(entriesClone.indexOf(found), 1)
     }
-
     return entriesClone;
   })
 )
+
+export const metaReducerLocalStorage = (reducer: ActionReducer<any>): ActionReducer<any> => {
+  return (state, action) => {
+    if (action.type === INIT || action.type == UPDATE) {
+      const storageValue = localStorage.getItem("state");
+      if (storageValue) {
+        try {
+          return JSON.parse(storageValue);
+        } catch {
+          localStorage.removeItem("state");
+        }
+      }
+    }
+    const nextState = reducer(state, action);
+    localStorage.setItem("state", JSON.stringify(nextState));
+    return nextState;
+  };
+};
